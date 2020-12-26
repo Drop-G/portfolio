3188a01c4b7da5b013327baa0742a4b617b10d61
Ted Peters <44210809+Drop-G@users.noreply.github.com>
2020-12-26 11:31:46 -0500

    projects updated with styled links and increased size in images.

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -6,21 +6,14 @@ class Portfolio extends Component {
     if (this.props.data) {
       var projects = this.props.data.projects.map(function (projects) {
         var projectImage = 'images/portfolio/' + projects.image;
-        
+
         return (
-          <div key={projects.title} className="columns portfolio-item">
-            <div className="item-wrap">
-              <a href={projects.url} title={projects.title}>
-                <img alt={projects.title} src={projectImage} />
-                <div className="overlay">
-                  <div className="portfolio-item-meta">
-                    <h5>{projects.title}</h5>
-                    <p>{projects.category}</p>
-                  </div>
-                </div>
-              </a>
-            </div>
-            <a href = {projects.live}>Live Link</a>
+          <div key={projects.title}>
+              <img alt={projects.title} src={projectImage} />
+              <ul className="text-center">
+                <a href={projects.live} className="button btn project-btn "><i className="fa fa-book"></i> {projects.title}</a>
+                <a href={projects.url} className="button btn github-btn "><i className="fa fa-github"></i>Project Repo</a>
+              </ul>
           </div>
         )
       })
